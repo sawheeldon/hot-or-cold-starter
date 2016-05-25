e2541b6cfda6b243f048fa2dafabe9ed0216eb86
sawheeldon <simon@jampot.design>
2016-05-25 20:51:13 +0100

    Try

--- js/app.js
+++ js/app.js
@@ -1,16 +1,54 @@
+//STEP ONE - Declare Global Variable **Random Number**
+
+var randomNumber = generateRandomNumber (1,100);
+
+
+//STEP TWO - functions declarations
+
+//function that validates the number inputted
+
+function numberInput(){
+
+}
+
+//function show whether hot or cold
+
+function hotOrCold(){
+
+}
+
+//function guess history
+
+function guessHistory(){
+
+}
+
+//function guess counter
+
+function guessCounter(){
+
+}
+
+//function new game
+
+function newGame(){
+
+}
+
+//STEP THREE - function usage
 
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
 
-  	});
+    /*--- Display information modal box ---*/
+      $(".what").click(function(){
+        $(".overlay").fadeIn(1000);
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+      });
+
+      /*--- Hide information modal box ---*/
+      $("a.close").click(function(){
+          $(".overlay").fadeOut(1000);
+      });
 
 });
 
